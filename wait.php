f40b62bbdac5e49efb20c7b33300cf8fb976f0e1
Haniwasanndayo <33508020+Haniwasanndayo@users.noreply.github.com>
2019-06-17 18:54:05 +0900

    Update wait.php

--- wait.php
+++ wait.php
@@ -1,11 +1,11 @@
 <?php
 		while(true){						//以下を無限ループする
 		//$status ← クラウドサーバからledstatus.txtを取得
-		$status = file_get_contents("https://hanihanitest.azurewebsites.net/getLEDStatus.php");
+		$status = file_get_contents("hoge/getLEDStatus.php");
 		
 		if($status == 1){				//「1」(ON)ならば
 			exec("python3 bazirisuku.py");
-			$get = file_get_contents("https://hanihanitest.azurewebsites.net/ledstatuszero.php");
+			$get = file_get_contents("hoge/ledstatuszero.php");
 		}
 		
 		usleep(10);					//10秒待つ
